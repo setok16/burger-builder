fbd33d58d46191b5358943d154cebd96c5d1ccba
Kotaro Seto <kotaro.s@ludovico.jp>
2018-10-03 14:52:03 +0900

    Add pseudo-dynamic ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients) // Gives array of keys
+    .map(igKey => { // Create 2D array
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />
+      });
+    })
+    .reduce((arr, element) => arr.concat(element), []); // Flatten array
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+    }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
