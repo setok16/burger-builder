027c062fadef7177fcd2e07bfc4a75443e421024
Kotaro Seto <kotaro.s@ludovico.jp>
2018-10-04 16:58:35 +0900

    Create side-drawer.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button clicked={props.purchaseCancelled} btnType="Danger">CANCEL</Button>
       <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
